5de2f1d9899ad94db9d0c9f993e447caff2a6405
ensar <ensar.makas@gmail.com>
2022-12-15 09:22:59 +0300

    created service results

---
+++ shop_app.contract/ServiceResults/SuccessStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shop_app.contract.ServiceResults
+{
+    public class SuccessStatus<T>: ServiceResult<T>
+    {
+    }
+}
