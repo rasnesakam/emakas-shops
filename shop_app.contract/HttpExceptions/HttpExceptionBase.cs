edff63a0f6f86802a2d02d39b015671967ea55c4
ensar <ensar.makas@gmail.com>
2022-12-15 09:22:08 +0300

    created http exceptions

---
+++ shop_app.contract/HttpExceptions/HttpExceptionBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shop_app.contract.HttpExceptions
+{
+    public class HttpExceptionBase: Exception
+    {
+        public int StatusCode { get; set; }
+
+        public HttpExceptionBase(int statusCode)
+        {
+            StatusCode = statusCode;
+        }
+
+        public HttpExceptionBase(string? message) : base(message)
+        {
+        }
+
+        public HttpExceptionBase(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
