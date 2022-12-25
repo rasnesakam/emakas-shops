a9e1c2d6d90ed521e3dc96783719b100c60774d8
ensar <ensar.makas@gmail.com>
2022-12-25 19:11:40 +0300

    Repository added to abstract unit of work interface

--- shop-app.data/Abstract/IUnitOfWork.cs
+++ shop-app.data/Abstract/IUnitOfWork.cs
@@ -12,6 +12,7 @@ namespace shop_app.data.Abstract
         ICategoryRepository CategoryRepository { get; }
         IOrderRepository OrdersRepository { get; }
         IAddressRepository AddressRepository { get; }
+		IPropertyRepository PropertyRepository { get; }
         IRepositoryBase<T> GetRepository<T>() where T: class;
         
 
