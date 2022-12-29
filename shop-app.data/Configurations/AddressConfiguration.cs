3d167126863b345b43724665dc4e2978522369c8
ensar <ensar.makas@gmail.com>
2022-12-30 01:17:15 +0300

    added configuration for address entity

--- shop-app.data/Configurations/AddressConfiguration.cs
+++ shop-app.data/Configurations/AddressConfiguration.cs
@@ -13,7 +13,15 @@ namespace shop_app.data.Configurations
     {
         public void Configure(EntityTypeBuilder<Address> builder)
         {
-            
+            builder.HasKey(a => a.Id);
+            builder.Property(a => a.Name).IsRequired().HasMaxLength(50);
+            builder.Property(a => a.AddressType).IsRequired().HasMaxLength(50);
+            builder.Property(a => a.AddressLine1).IsRequired().HasMaxLength(140);
+            builder.Property(a => a.AddressLine2).IsRequired(false).HasMaxLength(140);
+            builder.Property(a => a.Province).IsRequired().HasMaxLength(50);
+            builder.Property(a => a.City).IsRequired().HasMaxLength(50);
+            builder.Property(a => a.Country).IsRequired().HasMaxLength(50);
+            builder.Property(a => a.PostalCode).IsRequired().HasMaxLength(50);
         }
     }
 }
