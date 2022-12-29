89fa16b0eb60fcd0a9826d450f47501eec3b1a6e
ensar <ensar.makas@gmail.com>
2022-12-30 01:13:26 +0300

    new entity added. Review

---
+++ shop-app.entity/Review.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shop_app.entity
+{
+    public class Review
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }
+        public Guid CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public double Rate { get; set; }
+    }
+}
