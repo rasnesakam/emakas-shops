fc670974e9c8f2c2ba5e28e395b112e160ca5e59
ensar <ensar.makas@gmail.com>
2023-01-14 23:01:52 +0300

    Edited uri getter of the product. Will be reconsidered again

--- shop-app.entity/Product.cs
+++ shop-app.entity/Product.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace shop_app.entity
@@ -15,7 +16,32 @@ namespace shop_app.entity
 
 		public string ImageUrl {get;set;}
 
-		public string Uri {get;set;} //[lowercased-dashed-name]-[product-id]
+		public string Uri
+		{
+			get
+			{
+				Regex pattern = new Regex("[Ç|ç|Ğ|ğ|İ|ı|Ö|ö|Ş|ş|Ü|ü|A-Z| ]");
+				var replaced = pattern.Replace(Name, m =>
+				{
+					if (m.Value == "Ç" || m.Value == "ç")
+						return "c";
+					if (m.Value == "Ğ" || m.Value == "ğ")
+						return "g";
+					if (m.Value == "İ" || m.Value == "ı")
+						return "i";
+					if (m.Value == "Ö" || m.Value == "ö")
+						return "o";
+					if (m.Value == "Ş" || m.Value == "ş")
+						return "s";
+					if (m.Value.Equals("Ü") || m.Value.Equals("ü"))
+						return "u";
+					if (m.Value == " ")
+						return "-";
+					return m.Value.ToLower();
+				});
+				return replaced + Id.ToString().Split('-')[-1];
+			}
+		} //[lowercased-dashed-name]-[product-id]
 
 		public decimal Price {get;set;}
 
