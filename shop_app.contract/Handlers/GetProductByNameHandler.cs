09f0a4fe12543ae523b1e2af179b95813c1705a9
ensar <ensar.makas@gmail.com>
2023-01-19 13:23:16 +0300

    Created handler for request that gets product by it's name

--- shop_app.contract/Handlers/GetProductByNameHandler.cs
+++ shop_app.contract/Handlers/GetProductByNameHandler.cs
@@ -1,6 +1,32 @@
+using MediatR;
+using shop_app.contract.Requests.Queries;
+using shop_app.contract.ServiceResults;
+using shop_app.data.Abstract;
+using shop_app.entity;
+using shop_app.service.Abstract;
+using shop_app.shared.Utilities.Results.ComplexTypes;
+
 namespace shop_app.contract.Handlers;
 
-public class GetProductByNameHandler
+public class GetProductByNameHandler: IRequestHandler<GetProductByNameRequest, ServiceResult<Product>>
 {
-    
+    private IProductService _service;
+
+    public GetProductByNameHandler(IProductService service)
+    {
+        _service = service;
+    }
+
+    public async Task<ServiceResult<Product>> Handle(GetProductByNameRequest request, CancellationToken cancellationToken)
+    {
+        var response = await _service.GetAllBy(p => p.Name == request.Name);
+        switch (response.Status)
+        {
+            case ResultStatus.Success:
+                return new SuccessStatus<Product>(response.Payload.First());
+            case ResultStatus.NotFound:
+                return new NotFoundErrorResult<Product>();
+            default: return new InternalServerErrorResult<Product>();
+        }
+    }
 }
