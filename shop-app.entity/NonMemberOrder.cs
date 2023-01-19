ba87025d9342e91d67bc6fe2a270aacd9daaa0e5
ensar <ensar.makas@gmail.com>
2023-01-19 13:11:38 +0300

    Added new type of order. Non member's order will be stored here.

--- shop-app.entity/NonMemberOrder.cs
+++ shop-app.entity/NonMemberOrder.cs
@@ -1,6 +1,12 @@
+using System;
+
 namespace shop_app.entity;
 
-public class NonMemberOrder
+public class NonMemberOrder: Order
 {
-    
+    public string CustomerName { get; set; }
+    public string CustomerSurname { get; set; }
+    public string CustomerMiddleName { get; set; }
+    public string CustomerMail { get; set; }
+    public string CustomerPhone { get; set; }
 }
