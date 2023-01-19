3e2c5fb09464fb14c87c734fccc443782c7fda22
ensar <ensar.makas@gmail.com>
2023-01-19 13:21:49 +0300

    Added searhing for products by name

--- shop_app.contract/Handlers/SearchProductByNameHandler.cs
+++ shop_app.contract/Handlers/SearchProductByNameHandler.cs
@@ -1,6 +1,35 @@
+using System.Collections;
+using MediatR;
+using shop_app.contract.Requests.Queries;
+using shop_app.contract.ServiceResults;
+using shop_app.data.Abstract;
+using shop_app.entity;
+using shop_app.service.Abstract;
+using shop_app.shared.Utilities.Results.ComplexTypes;
+
 namespace shop_app.contract.Handlers;
 
-public class SearchProductHandler
+public class SearchProductByNameHandler: IRequestHandler<SearchProductsByName,ServiceResult<IEnumerable<Product>>>
 {
+    private IProductService _service;
     
+    public SearchProductByNameHandler(IProductService service)
+    {
+        _service = service;
+    }
+
+
+    public async Task<ServiceResult<IEnumerable<Product>>> Handle(SearchProductsByName request, CancellationToken cancellationToken)
+    {
+        var response = await _service.GetAllBy(p => p.Name.Contains(request.Name));
+        switch (response.Status)
+        {
+            case ResultStatus.Success:
+                return new SuccessStatus<IEnumerable<Product>>(response.Payload);
+            case ResultStatus.NotFound:
+                return new NotFoundErrorResult<IEnumerable<Product>>();
+            default:
+                return new InternalServerErrorResult<IEnumerable<Product>>();
+        }
+    }
 }
