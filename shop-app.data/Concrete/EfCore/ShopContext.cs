5d4145d2c7b9b09a5aed785ebea337e30b483467
ensar <ensar.makas@gmail.com>
2023-01-19 13:06:09 +0300

    Context set to Seller and IdentityRole, removed sets bc they are in unit of work. Added new configurations

--- shop-app.data/Concrete/EfCore/ShopContext.cs
+++ shop-app.data/Concrete/EfCore/ShopContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
@@ -10,38 +11,29 @@ using shop_app.data.Configurations;
 using shop_app.entity;
 namespace shop_app.data.Concrete.EfCore
 {
-    public class ShopContext: IdentityDbContext<User,Role,Guid>
+    public class ShopContext: IdentityDbContext<Seller,IdentityRole<Guid>,Guid>
     {
 
         public ShopContext(DbContextOptions options): base(options)
         {
-            
         }
-
-        public DbSet<Product> Products { get; set; }
-
-        public DbSet<Category> Categories { get; set; }
-
-        public DbSet<Order> Orders{ get; set; }
-
-        /*
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseNpgsql("Server=137.184.153.72;Port=5445;Database=asp_main;User Id=root;Password=r00T1453!;");
-
-        }
-        */
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            
+            modelBuilder.ApplyConfiguration(new SellerConfiguration());
+            modelBuilder.ApplyConfiguration(new AddressConfiguration());
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            //modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+            //modelBuilder.ApplyConfiguration(new OrderConfiguration());
             modelBuilder.ApplyConfiguration(new ProductCategoryConfiguration());
-            modelBuilder.ApplyConfiguration(new UserConfiguration());
-            modelBuilder.ApplyConfiguration(new RoleConfiguration());
-            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductImageConfiguration());
+            modelBuilder.ApplyConfiguration(new PropertyConfiguration());
+            modelBuilder.ApplyConfiguration(new ReviewConfiguration());
+            //modelBuilder.ApplyConfiguration(new RoleConfiguration());
+            //modelBuilder.ApplyConfiguration(new UserConfiguration());
         }
     }
 }
