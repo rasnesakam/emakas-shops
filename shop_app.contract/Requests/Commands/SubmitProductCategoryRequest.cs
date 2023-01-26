e432646d0ef912bcbc5182c8ba220d7cdcc433b8
ensar <ensar.makas@gmail.com>
2023-01-26 13:21:46 +0300

    Created new request for submitting productCategory

--- shop_app.contract/Requests/Commands/SubmitProductCategoryRequest.cs
+++ shop_app.contract/Requests/Commands/SubmitProductCategoryRequest.cs
@@ -1,6 +1,10 @@
+using MediatR;
+using shop_app.contract.ServiceResults;
+using shop_app.entity;
+
 namespace shop_app.contract.Requests.Commands;
 
-public class SubmitProductCategory
+public class SubmitProductCategoryRequest: IRequest<ServiceResult<ProductCategory>>
 {
     
 }
