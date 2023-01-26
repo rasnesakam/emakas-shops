926340cc2ee9a3493756d27f439269a2a595baec
ensar <ensar.makas@gmail.com>
2023-01-26 13:17:43 +0300

    Updated getting by category and added search function

--- shop-app.data/Concrete/EfCore/EfCoreProductRepository.cs
+++ shop-app.data/Concrete/EfCore/EfCoreProductRepository.cs
@@ -3,6 +3,7 @@ using shop_app.data.Abstract;
 using shop_app.data.Exceptions;
 using shop_app.entity;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,12 +29,21 @@ namespace shop_app.data.Concrete.EfCore
         public async Task<IEnumerable<Product>> GetAllByCategory(Category category)
         {
             IEnumerable<Product> products = await _dbContext.Set<Product>()
-                .Where(p => p.ProductCategories.Any(pc => pc.Category.URI == category.URI)).ToListAsync();
+                .Where(p => p.ProductCategories.Any(pc => pc.Category.URI == category.URI))
+                .Include(p => p.ProductImages ).ToListAsync();
             if (products.Any())
                 return products;
-            else throw new NoElementFoundException($"Element couldn't found with category {category.Name ?? category.URI}");
+            throw new NoElementFoundException($"Element couldn't found with category {category.Name ?? category.URI}");
         }
 
+        public async Task<IEnumerable<Product>> Search(string search)
+        {
+            IEnumerable<Product> products = await _dbContext.Set<Product>()
+                .Where(p => EF.Functions.Like(p.Name,$"%{search}%")).ToListAsync();
+            if (products.Any())
+                return products;
+            throw new NoElementFoundException();
+        }
         
     }
 }
