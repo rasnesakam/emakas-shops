cdee7d99ea540c61c6f27edf03e5de0934ba7e7a
ensar <ensar.makas@gmail.com>
2023-01-26 13:20:27 +0300

    Added mediator for submitting product

--- shop_app.contract/Requests/Commands/SubmitProductRequest.cs
+++ shop_app.contract/Requests/Commands/SubmitProductRequest.cs
@@ -1,6 +1,11 @@
+using MediatR;
+using shop_app.contract.DTO;
+using shop_app.contract.ServiceResults;
+using shop_app.entity;
+
 namespace shop_app.contract.Requests.Commands;
 
-public class SubmitProductRequest
+public class SubmitProductRequest: IRequest<ServiceResult<Product>>
 {
-    
+    public Product Product { get; set; }
 }
