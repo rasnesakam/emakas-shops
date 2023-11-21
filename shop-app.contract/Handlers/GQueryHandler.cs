a7e91b85869b487459dc261a578a544d9eec6807
Ensar Makas <ensar.makas@gmail.com>
2023-11-22 01:42:00 +0300

    Added Query requests and handlers in more generic way

--- shop-app.contract/Handlers/GQueryHandler.cs
+++ shop-app.contract/Handlers/GQueryHandler.cs
@@ -1,6 +1,29 @@
+using MediatR;
+using shop_app.contract.Requests.Queries;
+using shop_app.contract.ServiceResults;
+using shop_app.service.Abstract;
+using shop_app.shared.Utilities.Results.ComplexTypes;
+
 namespace shop_app.contract.Handlers;
 
-public class GQueryHandler
+public class GQueryHandler<TEntity>: IRequestHandler<GQueryRequest<TEntity>,ServiceResult<IEnumerable<TEntity>>>
+    where TEntity: class, new()
 {
-    
+    private readonly IServiceBase<TEntity> _service;
+
+    public GQueryHandler(IServiceBase<TEntity> service)
+    {
+        _service = service;
+    }
+
+    public async Task<ServiceResult<IEnumerable<TEntity>>> Handle(GQueryRequest<TEntity> request, CancellationToken cancellationToken)
+    {
+        var result = await _service.GetAllBy(request.Predicate ?? (entity => true) , request.Included! );
+        return result.Status switch
+        {
+            ResultStatus.Success => new SuccessStatus<IEnumerable<TEntity>>(result.Payload),
+            ResultStatus.NotFound => new NotFoundErrorResult<IEnumerable<TEntity>>("Item Not Found"),
+            _ => new InternalServerErrorResult<IEnumerable<TEntity>>(result.Message, result.Exception)
+        };
+    }
 }
--- shop-app.contract/Requests/Queries/GQueryRequest.cs
+++ shop-app.contract/Requests/Queries/GQueryRequest.cs
@@ -1,6 +1,13 @@
+using System.Linq.Expressions;
+using MediatR;
+using shop_app.contract.ServiceResults;
+
 namespace shop_app.contract.Requests.Queries;
 
-public class GQueryRequest
+public class GQueryRequest<TEntity>: IRequest<ServiceResult<IEnumerable<TEntity>>>
+    where TEntity: class, new()
 {
-    
+    public Expression<Func<TEntity, bool>>? Predicate;
+    public Expression<Func<TEntity, object>>[]? Included;
+
 }
